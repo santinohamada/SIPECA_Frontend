4059647c8eacf17262ba01d094a36371cc6cbb1c
SantinoHamada <hamadasantino@hotmail.com>
2025-06-02 18:54:54 -0300

    con colorcito

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,10 @@ export default function Home() {
   };
 
   return (
-    <main className="container mx-auto py-6 px-4">
+    <div className="bg-green-300 flex min-h-full">
+
+    
+    <main className="container mx-auto py-6 px-4 ">
       <h1 className="text-3xl font-bold text-center mb-8">
         SIPECA - SIMULADOR DE PERAS Y CARPOCAPSA
       </h1>
@@ -182,5 +185,6 @@ export default function Home() {
         </div>
       </div>
     </main>
+    </div>
   );
 }
